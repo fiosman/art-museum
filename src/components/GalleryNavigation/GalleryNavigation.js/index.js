b5204d1a55c92cf00b391aa93eae1daf7d735393
fiosman <fares.osman@outlook.com>
2021-12-03 23:52:23 -0500

    Adds some changes to the gallerynavigation component

--- src/components/GalleryNavigation/GalleryNavigation.js/index.js
+++ src/components/GalleryNavigation/GalleryNavigation.js/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const GalleryNavigation = ({ ...galleries }) => {
-  return <NavLink></NavLink>;
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <h1>Galleries </h1>
+    </nav>
+  );
 };
 
 export default GalleryNavigation;
