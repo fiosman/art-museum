3c2331b597381efdcf251feba54e7bddbd13bbd9
fiosman <fares.osman@outlook.com>
2021-12-04 01:15:23 -0500

    Boldface active links

--- src/components/GalleryNavigation/GalleryNavigation.js/index.js
+++ src/components/GalleryNavigation/GalleryNavigation.js/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./GalleryNavigation.css";
 const GalleryNavigation = ({ galleries }) => {
   const buildGalleries = () =>
     galleries.map((gallery, idx) => (
