b4bc40e77442e3f8db93ec99e212ba1e8d54b441
fiosman <fares.osman@outlook.com>
2021-12-04 01:12:33 -0500

    LRender galleries

--- src/components/GalleryNavigation/GalleryNavigation.js/index.js
+++ src/components/GalleryNavigation/GalleryNavigation.js/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-const GalleryNavigation = ({ ...galleries }) => {
+const GalleryNavigation = ({ galleries }) => {
+  const buildGalleries = () =>
+    galleries.map((gallery, idx) => (
+      <NavLink key={idx} to={`/galleries/${gallery.galleryid}`}>
+        {gallery.name}
+      </NavLink>
+    ));
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
       <h1>Galleries </h1>
+      {buildGalleries()}
     </nav>
   );
 };
